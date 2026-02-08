9fcfed0166925f29d1e14ac51acf1e4aafa3978b
VICTORY OF CALVARY <phlagongkk@gmail.com>
2026-02-08 17:34:25 +0000

    1. Fix the "Blank Page" (Add .nojekyll)
    You must tell GitHub to skip Je

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  /* config options here */
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
