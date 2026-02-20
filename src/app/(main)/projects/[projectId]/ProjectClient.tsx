1db5e300ded8dfd045eb54d35231f095a74e672f
VICTORY OF CALVARY <phlagongkk@gmail.com>
2026-02-20 13:00:27 +0000

    this element and logic should be moved down where the text in the image

--- src/app/(main)/projects/[projectId]/ProjectClient.tsx
+++ src/app/(main)/projects/[projectId]/ProjectClient.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState } from 'react';
@@ -174,7 +175,7 @@ export default function ProjectClient({ project, placeholderImages }: { project:
                                   setRapidoScreen('flight');
                                   setTransitResults(null);
                                 }}
-                                className="absolute top-[17.2%] left-[58%] w-[30%] h-[6%] bg-transparent cursor-pointer z-[95]"
+                                className="absolute top-[43%] left-[53%] w-[35%] h-[15%] bg-transparent cursor-pointer z-[95]"
                                 title="Flight Booking"
                               />
                             )}
