5d8ba0275bce50274380cbaded0b884664721492
VICTORY OF CALVARY <phlagongkk@gmail.com>
2026-02-21 04:45:12 +0000

    this button for "YES" move it upward (_for element button_)

--- src/app/(main)/projects/[projectId]/ProjectClient.tsx
+++ src/app/(main)/projects/[projectId]/ProjectClient.tsx
@@ -307,7 +307,7 @@ export default function ProjectClient({ project, placeholderImages }: { project:
                               <>
                                 <button 
                                   onClick={() => setRapidoScreen('auto-find')}
-                                  className="absolute top-[21%] left-1/2 -translate-x-1/2 w-[60%] h-[5%] bg-transparent cursor-pointer z-[90]"
+                                  className="absolute top-[8%] left-1/2 -translate-x-1/2 w-[60%] h-[5%] bg-transparent cursor-pointer z-[90]"
                                   aria-label="YES - Confirm Offline"
                                 />
                                 <button 
