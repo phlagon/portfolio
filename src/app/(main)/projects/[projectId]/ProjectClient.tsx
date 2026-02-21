0d24517d82417d7606b355d60e4e51b3573031ae
VICTORY OF CALVARY <phlagongkk@gmail.com>
2026-02-21 02:53:11 +0000

    Try fixing this error: `Runtime Error: Element type is invalid: expected

--- src/app/(main)/projects/[projectId]/ProjectClient.tsx
+++ src/app/(main)/projects/[projectId]/ProjectClient.tsx
@@ -3,7 +3,7 @@
 
 import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
-import Link from 'next/navigation';
+import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import { 
   ArrowLeft, 
